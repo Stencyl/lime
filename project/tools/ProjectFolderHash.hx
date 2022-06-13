60df15af1a1fb0ebb945d63490d8dd156810eeeb
Justin Espedal <justin.espedal@gmail.com>
2022-06-13 23:57:19 +0900

    Update project folder hash path filter

--- project/tools/ProjectFolderHash.hx
+++ project/tools/ProjectFolderHash.hx
@@ -3,6 +3,9 @@ package;
 import sys.FileSystem;
 import fsindexer.tree.IndexedFileTree;
 import fsindexer.filter.PathFilter;
+import fsindexer.filter.PathVisitResult;
+
+using StringTools;
 
 class ProjectFolderHash
 {
@@ -13,11 +16,34 @@ class ProjectFolderHash
 		if(FileSystem.exists(indexFile))
 			FileSystem.deleteFile(indexFile);
 
-		var pathsToExclude = ["lib" => "", "obj" => "", "tools" => ""];
-		var excludeFilter = PathFilters.fromExcludePredicate(path -> pathsToExclude.exists(path));
-		
-		var sourceTree = IndexedFileTree.loadWithFilter(projectFolderPath, indexFile, excludeFilter);
+		var sourceTree = IndexedFileTree.loadWithFilter(projectFolderPath, indexFile, new ProjectFolderPathFilter());
 		
 		trace(sourceTree.getRoot().getHash());
 	}
+}
+
+class ProjectFolderPathFilter implements PathFilter
+{
+	public function new() {}
+
+	public function requiresTraversal():Bool
+		return false;
+	
+	public function preVisitDirectory(path:String):PathVisitResult
+	{
+		if(path == "obj" || path == "tools")
+			return EXCLUDE;
+		if(path.startsWith("lib/") && !path.startsWith("lib/custom"))
+			return EXCLUDE;
+		return INCLUDE;
+	}
+
+	public function visitFile(path:String):PathVisitResult
+	{
+		if(path.endsWith("project.index") || path.endsWith("README.md") || path.endsWith(".DS_Store"))
+			return EXCLUDE;
+		return INCLUDE;
+	}
+
+	public function postVisitDirectory(path:String):Void {}
 }
