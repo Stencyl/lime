a63820010f1390a18903a5aae7c8e67254d4e299
Justin Espedal <justin.espedal@gmail.com>
2026-05-02 21:52:15 +0900

    Adjust ProjectFolderHash to pick up files in lib folder

--- project/tools/ProjectFolderHash.hx
+++ project/tools/ProjectFolderHash.hx
@@ -4,6 +4,8 @@ import sys.FileSystem;
 import fsindexer.tree.IndexedFileTree;
 import fsindexer.filter.PathFilter;
 
+using StringTools;
+
 class ProjectFolderHash
 {
 	static function main():Void
@@ -13,8 +15,17 @@ class ProjectFolderHash
 		if(FileSystem.exists(indexFile))
 			FileSystem.deleteFile(indexFile);
 
-		var pathsToExclude = ["lib" => "", "obj" => "", "tools" => ""];
-		var excludeFilter = PathFilters.fromExcludePredicate(path -> pathsToExclude.exists(path));
+		var excludeFilter = PathFilters.fromExcludePredicate(path -> {
+			if(path == "obj" || path == "tools" || path == indexFile || path == "BuildHashlink.xml" || path == "README.md") return true;
+			if(path.endsWith(".DS_Store")) return true;
+			//for direct descendants of lib/
+			if(path.startsWith("lib/") && path.indexOf("/", 4) == -1)
+			{
+				//don't ignore custom or *.xml, ignore everything else (lib folders)
+				return !path.endsWith("/custom") && !path.endsWith(".xml");
+			}
+			return false;
+		});
 		
 		var sourceTree = IndexedFileTree.loadWithFilter(projectFolderPath, indexFile, excludeFilter);
 		
